595dd4028e7e026377763fe196898b8185649d61
kccpac <colin_ng@hotmail.com>
2015-05-22 21:40:17 -0400

    First commit

---
+++ src/com/example/albumactivity/AlbumActivity.java
@@ -0,0 +1,128 @@
+package com.example.albumactivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+//import android.widget.FrameLayout.LayoutParams;
+import android.widget.GridView;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+//import android.widget.RelativeLayout.LayoutParams;
+
+public class AlbumActivity extends Activity {
+
+	private static final String Tag = AlbumActivity.class.getCanonicalName();
+	
+    private Integer[] mImageIds = {
+    		 R.drawable.sample_0,
+            R.drawable.sample_1,
+            R.drawable.sample_2,
+            R.drawable.sample_3,
+            R.drawable.sample_4,
+            R.drawable.sample_5,
+            R.drawable.sample_6,
+            R.drawable.sample_7
+    };
+
+	private GridView mGridView;
+
+	private AlbumAdapter mAdapter;
+	protected List<Integer> mAlbumList;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_album);
+		HorizontalScrollView  v = (HorizontalScrollView) findViewById(R.id.horizontalScrollView1);
+	DisplayMetrics metrics = getResources().getDisplayMetrics();
+		Log.i(Tag, "density:" + metrics.density + " widthPixel: "+metrics.widthPixels + " heightPixel: "+metrics.heightPixels);
+		mAlbumList = new ArrayList<Integer>();
+		mAdapter = new AlbumAdapter(mAlbumList);
+		mGridView = (GridView)findViewById(R.id.gridView1);
+		mGridView.setAdapter(mAdapter);
+	
+	//	android.view.ViewGroup.LayoutParams params;
+		//params = LayoutParams.
+		//LayoutParams params = 
+		//v.setLayoutParams);
+		LayoutInflater inflater;
+
+	    inflater=getLayoutInflater();
+	    LinearLayout inLay=(LinearLayout) findViewById(R.id.innerLay);
+
+		for (int id: mImageIds)
+		{
+			ImageView imv = new ImageView(this);
+			imv.setImageResource(id);	
+			imv.setId(id);
+			imv.setOnClickListener(new OnClickListener()
+			{
+	
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					Log.i(Tag, "view " + v.getId());
+					Log.i(Tag, "dimension (" + v.getWidth() + "," + v.getHeight() + ")");
+					if (v.getAlpha() <= 0.2F)
+					{
+						v.setAlpha(1.0F);
+						if (mAlbumList.contains(v.getId()))
+						{
+							mAlbumList.remove((Object)v.getId());
+						}						
+					}
+					else
+					{
+						v.setAlpha(0.2F);
+						mAlbumList.add(v.getId());
+					}
+					mAdapter.notifyDataSetChanged();				
+				}			
+			});
+			inLay.addView(imv);
+		}
+
+//		vv.setColumnWidth(columnWidth);
+		
+	//	adapter.registerDataSetObserver(new TestAlbum());
+	//	adapter.notifyDataSetChanged--
+		
+	//	adapter.unregisterDataSetObserver(null);
+		v.setHorizontalScrollBarEnabled(true);
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.album, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+}
---
+++ src/com/example/albumactivity/AlbumAdapter.java
@@ -0,0 +1,66 @@
+package com.example.albumactivity;
+
+import java.util.List;
+
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridLayout.LayoutParams;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
+
+public class AlbumAdapter extends BaseAdapter
+{
+
+	 private static final String Tag = AlbumAdapter.class.getCanonicalName();
+	List<Integer> mAlbumList;
+	public AlbumAdapter( List<Integer> albumList)
+	{
+		super();
+		mAlbumList = albumList;
+		
+	}
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		Log.i(Tag, "getCount " +  mAlbumList.size());
+		return mAlbumList.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		Log.i(Tag, "position");
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		Log.i(Tag, "getItemId " + position);
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		Log.i(Tag, "getView " + position);
+		
+		ImageView imv = new ImageView(parent.getContext());
+		int id = mAlbumList.get(position);
+		imv.setImageResource(id);	
+		imv.setId(id);
+//		LayoutParams params = new LayoutParams();
+//		params.height = params.width = 40;
+//		imv.setLayoutParams(params);
+		imv.setScaleType(ScaleType.FIT_XY);
+		return imv;
+	}
+	
+	public void getDetail()
+	{
+		Log.i(Tag, "getDetail ");
+	}
+}
+	
